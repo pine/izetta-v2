056162a9aee5cde7b56f860471a0046731eb108b
Pine Mizune <pinemz@gmail.com>
2019-06-05 11:53:29 +0900

    refactor: use external library

--- app/src/main/java/moe/pine/izetta/config/GitHubContributionsConfig.java
+++ app/src/main/java/moe/pine/izetta/config/GitHubContributionsConfig.java
@@ -1,18 +1,25 @@
 package moe.pine.izetta.config;
 
+import moe.pine.github.contribution.stats.ContributionStatsClient;
 import moe.pine.izetta.github.contributions.GitHubContributions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Nonnull;
 
 @Configuration
 public class GitHubContributionsConfig {
+    private static final int MAX_ATTEMPTS = 5;
+
+    @Bean
+    public ContributionStatsClient contributionStatsClient() {
+        return new ContributionStatsClient();
+    }
+
     @Bean
     public GitHubContributions githubContributions(
-        @Nonnull final RestTemplate restTemplate
+        @Nonnull final ContributionStatsClient contributionStatsClient
     ) {
-        return new GitHubContributions(restTemplate);
+        return new GitHubContributions(contributionStatsClient, MAX_ATTEMPTS);
     }
 }
--- lib/github-contributions/src/main/java/moe/pine/izetta/github/contributions/GitHubContributions.java
+++ lib/github-contributions/src/main/java/moe/pine/izetta/github/contributions/GitHubContributions.java
@@ -1,37 +1,41 @@
 package moe.pine.izetta.github.contributions;
 
-import com.google.common.annotations.VisibleForTesting;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.Pair;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import moe.pine.github.contribution.stats.Contribution;
+import moe.pine.github.contribution.stats.ContributionStats;
+import moe.pine.github.contribution.stats.ContributionStatsClient;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class GitHubContributions {
-    private static final String ENDPOINT = "https://github.com/users/%s/contributions";
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    @Nonnull
+    private final ContributionStatsClient contributionStatsClient;
 
-    private final RestTemplate restTemplate;
+    @Nonnull
+    private final RetryTemplate retryTemplate;
 
     public GitHubContributions(
-        @Nonnull final RestTemplate restTemplate
+        @Nonnull final ContributionStatsClient contributionStatsClient,
+        final int maxAttempts
     ) {
-        this.restTemplate = Objects.requireNonNull(restTemplate);
+        this.contributionStatsClient = Objects.requireNonNull(contributionStatsClient);
+        this.retryTemplate = new RetryTemplate() {{
+            final Map<Class<? extends Throwable>, Boolean> retryableExceptions =
+                Collections.singletonMap(IOException.class, true);
+            final RetryPolicy retryPolicy =
+                new SimpleRetryPolicy(maxAttempts, retryableExceptions);
+
+            setRetryPolicy(retryPolicy);
+        }};
     }
 
     @Nonnull
@@ -41,64 +45,18 @@ public class GitHubContributions {
     ) {
         Objects.requireNonNull(username);
 
-        return parseBody(fetchBody(username));
-    }
-
-    @Nonnull
-    @VisibleForTesting
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    String fetchBody(
-        @Nonnull final String username
-    ) {
-        Objects.requireNonNull(username);
-
-        final String endpoint = String.format(ENDPOINT, username);
-        final HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(MediaType.TEXT_HTML));
-
-        final HttpEntity<?> request = new HttpEntity<>(null, headers);
-        final ResponseEntity<String> response =
-            restTemplate.exchange(endpoint, HttpMethod.GET, request, String.class);
-
-        final String body = response.getBody();
-        if (StringUtils.isEmpty(body)) {
-            throw new RuntimeException(
-                String.format(
-                    "A HTTP body is empty :: username=%s, endpoint=%s",
-                    username,
-                    endpoint));
+        final ContributionStats stats;
+        try {
+            stats = retryTemplate.execute(args ->
+                contributionStatsClient.collect(username));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
         }
 
-        return body;
-    }
-
-    @Nonnull
-    @VisibleForTesting
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    Map<LocalDate, Integer> parseBody(
-        @Nonnull final String body
-    ) {
-        Objects.requireNonNull(body);
-
-        final Document doc = Jsoup.parse(body);
-        final Elements elements = doc.select("rect");
-        return elements
+        return stats.getContributions()
             .stream()
-            .flatMap(element -> {
-                final String dateString = element.attr("data-date");
-                final String countString = element.attr("data-count");
-
-                if (StringUtils.isEmpty(dateString)) {
-                    return Stream.empty();
-                }
-                if (StringUtils.isEmpty(countString)) {
-                    return Stream.empty();
-                }
-
-                final LocalDate date = LocalDate.parse(dateString, FORMATTER);
-                final Integer count = Integer.valueOf(countString);
-                return Stream.of(Pair.of(date, count));
-            })
-            .collect(Collectors.toUnmodifiableMap(Pair::getKey, Pair::getValue));
+            .collect(Collectors.toUnmodifiableMap(
+                Contribution::getDate,
+                Contribution::getCount));
     }
 }
