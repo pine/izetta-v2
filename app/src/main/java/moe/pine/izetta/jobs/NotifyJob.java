26ce345c81c46880d0c8789fee6cd9db87e241b4
Pine Mizune <pinemz@gmail.com>
2019-06-22 19:29:05 +0900

    refactor: remove unnecessary code

--- app/src/main/java/moe/pine/izetta/jobs/NotifyJob.java
+++ app/src/main/java/moe/pine/izetta/jobs/NotifyJob.java
@@ -1,13 +1,11 @@
 package moe.pine.izetta.jobs;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class NotifyJob {
     @ConditionalOnProperty(value = "scheduling.enabled", havingValue = "true")
     @Scheduled(cron = "0 11 * * * *")
