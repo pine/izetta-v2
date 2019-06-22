4bf3731895bcdd27e5293b50c80b2847f9e27356
Pine Mizune <pinemz@gmail.com>
2019-06-22 17:33:07 +0900

    fix: remove sudo

--- .circleci/setup-retry.sh
+++ .circleci/setup-retry.sh
@@ -2,5 +2,5 @@
 
 set -eu -o pipefail
 
-sudo curl https://raw.githubusercontent.com/kadwanev/retry/master/retry -o /usr/bin/retry
-sudo chmod +x /usr/bin/retry
+curl https://raw.githubusercontent.com/kadwanev/retry/master/retry -o /usr/bin/retry
+chmod +x /usr/bin/retry
