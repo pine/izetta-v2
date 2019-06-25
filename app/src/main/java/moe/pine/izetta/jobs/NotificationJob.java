4ce94823d4424a82449c861822b544415807142c
Pine Mizune <pinemz@gmail.com>
2019-06-25 12:06:19 +0900

    fix: innocent

--- app/src/main/java/moe/pine/izetta/jobs/NotificationJob.java
+++ app/src/main/java/moe/pine/izetta/jobs/NotificationJob.java
@@ -17,9 +17,9 @@ public class NotificationJob {
     private final SlackService slackService;
 
     @ConditionalOnProperty(value = "scheduling.enabled", havingValue = "true")
-    @Scheduled(cron = " 0 22 * * * *")
-    @Scheduled(cron = " 0 23 * * * *")
-    @Scheduled(cron = "30 23 * * * *")
+    @Scheduled(cron = "0  0 22 * * *")
+    @Scheduled(cron = "0  0 23 * * *")
+    @Scheduled(cron = "0 30 23 * * *")
     @Retryable
     public void run() {
         final int contribution = contributionService.getContribution();
